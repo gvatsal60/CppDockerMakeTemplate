c815761c43d12d2733d1362a171b61caa40c0ba7
Vatsal Gupta <gvatsal60@Vatsals-Air.domain.name>
2024-05-20 13:53:15 +0530

    Fixed Project lowercase name issue

--- cfg/docker.mk
+++ cfg/docker.mk
@@ -14,7 +14,7 @@ DOCKER_GID ?= $(shell id -g)
 DOCKER_USER_ARG ?= --user $(DOCKER_UID):$(DOCKER_GID)
 
 # Project Settings (Change as per requirement)
-PROJECT_NAME := $(shell basename `git rev-parse --show-toplevel`)
+PROJECT_NAME := $(shell basename `git rev-parse --show-toplevel` | tr '[:upper:]' '[:lower:]')
 
 # Name of the Docker image based on the repository root directory name.
 DOCKER_BASE_IMG_NAME := $(PROJECT_NAME)
